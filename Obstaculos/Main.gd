f1f76bbc60fa316684afb3d9d07099cdf925db22
CausalTimeLoop <116341016+CausalTimeLoop@users.noreply.github.com>
2023-02-04 12:29:25 +0100

    Generacion Obstaculos
    Los obstaculos ahora se general fuera de  la vision del jugador

--- Obstaculos/Main.gd
+++ Obstaculos/Main.gd
@@ -14,15 +14,20 @@ func _on_ObstacleTimer_timeout():
 	var obstacle_spawn_location = get_node("ObstaclePath/ObstacleSpawnLocation")
 	obstacle_spawn_location.offset = randi()
 
-	# Set the obstacle's direction perpendicular to the path direction.S
+	# Set the mob's direction perpendicular to the path direction.
 	var direction = obstacle_spawn_location.rotation + PI / 2
 
 	# Set the obstacle's position to a random location.
 	obstacle.position = obstacle_spawn_location.position
 
+	# Add some randomness to the direction.
+	# Add some randomness to the direction.
+	direction += rand_range(-PI / 2, PI / 2)
+	obstacle.rotation = direction
+
 	# Choose the velocity for the obstacle.
-	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
-	obstacle.linear_velocity = velocity.rotated(direction)
+	#var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
+	#obstacle.linear_velocity = velocity.rotated(direction)
 
 	# Spawn the obstacle by adding it to the Main scene.
 	add_child(obstacle)
